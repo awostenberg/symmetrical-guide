026285b155181c9a12dee1d448cd3bb52ca51b5b
Alan Wostenberg <awostenberg@gmail.com>
2024-05-31 22:28:16 -0600

    F1** navbar loc 300 as given

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">daisyUI</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li><a>Link 1</a></li>
+              <li><a>Link 2</a></li>
+
+              <li>
+                <details>
+                  <summary>
+                    Parent
+                  </summary>
+                  <ul className="p-2 bg-base-100 rounded-t-none">
+                    <li><a>Link 1</a></li>
+                    <li><a>Link 2</a></li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {children}
+      </body>
     </html>
   );
 }

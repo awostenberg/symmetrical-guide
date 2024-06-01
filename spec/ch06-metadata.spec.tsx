c86df742f219ff13674491dfcd35d64d633a13e5
Alan Wostenberg <awostenberg@gmail.com>
2024-06-01 11:45:06 -0600

    F1** customize the metadata for about page loc.334

--- spec/ch06-metadata.spec.tsx
+++ spec/ch06-metadata.spec.tsx
@@ -4,4 +4,5 @@
             it.todo("has description")
             it.todo("has keywords")
           
+            it.todo("customize metadata for about page (loc 334)")
         })
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,11 @@
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "About Greg\'s Portfolio of Small Bets",
+    description: "Tech Courses and Books",
+    keywords: 'passive income, small bets, tech courses, tech books, tech tutorials',
+  };    //todo rm duplication loc 334
+
 const About = () => 
     
     (<div>About Page</div>);

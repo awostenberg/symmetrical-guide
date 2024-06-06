6552a186513dc84e3681486894c5ade83bfd8853
Alan Wostenberg <awostenberg@gmail.com>
2024-06-06 16:49:12 -0600

    t1 render repo name and description plain text loc.888

--- src/app/githubusers/[user]/page.tsx
+++ src/app/githubusers/[user]/page.tsx
@@ -1,9 +1,25 @@
 
 import Repos from '../../components/repos';
 
-const UserRepos = ( {params:{user}} ) => {
-    //const result:[any] = Repos({user:user});
-    return (<h1>User {user} Repo Page</h1>);
+const UserRepos = async ({ params: { user } }) => {
+    const result: [any] = await Repos({ user: user });
+    const name1 = result[0].name;
+    const description1 = result[0].description
+    return (
+        <div>
+            <h1>User {user} Repo Page</h1>
+            {result.map(item => (
+                <div>
+                    <p>{item.name}</p>
+                    <p>{item.description}</p>
+                </div>
+
+            ))}
+
+        
+        </div>
+
+    );
 };
 
 export default UserRepos;
--- src/app/githubusers/[user]/userrepo.spec.tsx
+++ src/app/githubusers/[user]/userrepo.spec.tsx
@@ -1,12 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import UserRepos from './page';
 
+ //todo loosen coupling: following data and mock coupled to component UserRepos calls. 
+import greg from '../../components/greg.spec.sample.json';
+
+import fetchMock from "jest-fetch-mock";
+fetchMock.enableMocks();
+
 describe('user repos page', () => {
-    it('renders the page', () => {
+    it('renders the page', async () => {
+
+        fetchMock.resetMocks();
+        fetchMock.mockResponseOnce(JSON.stringify(greg));
+
         const u = {'user':'greg'}
-        render(<UserRepos params={u} />)
+        const jsx = await UserRepos({'params':u}) 
+        render(jsx);
 
         expect(screen.getByRole('heading').textContent).toContain('User greg Repo Page')
 
+        //todo make it a table and check it's column header, table row
+        const paragraphs = screen.getAllByRole('paragraph').map(item=>item.textContent);;
+        expect(paragraphs[0]).toContain("CodableInterception");
+        expect(paragraphs[1]).toContain("A generalised library");
+       
     })
 })
